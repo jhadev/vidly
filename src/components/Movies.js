5fdff0efb91197981755ea3b5f149d91ed303848
JA <joshappeldev@gmail.com>
2018-12-05 11:22:03 -0500

    delete button

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,7 +6,10 @@ class Movies extends Component {
     movies: getMovies()
   };
 
-  handleDelete = movie => {};
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id)
+    this.setState({ movies: movies})
+  };
 
   render() {
     return (
@@ -17,15 +20,17 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
             <td>{movie.title}</td>
             <td>{movie.genre.name}</td>
             <td>{movie.numberInStock}</td>
             <td>{movie.dailyRentalRate}</td>
+            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btm-sm">Delete</button></td>
             </tr>
           ))}
         </tbody>
