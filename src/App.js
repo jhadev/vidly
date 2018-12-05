f23e82ca9973bea3efd012d98489b949c636234e
JA <joshappeldev@gmail.com>
2018-12-05 11:13:23 -0500

    table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Movies from './components/Movies'
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-      <h1>Hello</h1>
+      <Movies/>
       </main>
+
     );
   }
 }
