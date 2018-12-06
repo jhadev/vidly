068aec34de6cd8319ab141ce862494d4c796fa52
JA <joshappeldev@gmail.com>
2018-12-05 19:28:41 -0500

    jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/Movies'
+import Jumbotron from './components/Jumbotron'
 
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
+      <Jumbotron/>
       <main className="container">
       <Movies/>
       </main>
-
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Jumbotron extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div class="jumbotron jumbotron-fluid">
+    <h1 class="display-4 text-center">vidly</h1>
+    <p class="lead text-center">we got the movies</p>
+</div>
+     );
+  }
+}
+ 
+export default Jumbotron;
