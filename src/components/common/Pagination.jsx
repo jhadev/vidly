26b700d2ffa01d88993f7b1ca62232c40502cc8f
JA <joshappeldev@gmail.com>
2018-12-19 17:46:07 -0500

    pagination component

---
+++ src/components/common/Pagination.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import _ from "lodash";
+
+const Pagination = props => {
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
