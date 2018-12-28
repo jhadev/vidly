57a4e2ef7661eeecc468bac3b1448cc54ecc6e67
JA <joshappeldev@gmail.com>
2018-12-28 17:16:47 -0500

    react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movies from './components/Movies'
-import Jumbotron from './components/Jumbotron'
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/Movies";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/NotFound";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <Jumbotron/>
-      <main className="container">
-      <Movies/>
-      </main>
+        <Jumbotron />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
   }
