c0a55604b555525ab0c12be6cd51efefeab37b3c
JA <joshappeldev@gmail.com>
2019-01-01 16:20:47 -0500

    form events

--- src/components/common/LoginForm.jsx
+++ src/components/common/LoginForm.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log("Submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              id="password"
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
